74de51b909775ecfe2b351f50c6e1a0b4df552b9
digberi <orkepuma@gmail.com>
2021-12-20 18:32:42 +0200

    fix

--- src/components/slider-nft.tsx
+++ src/components/slider-nft.tsx
@@ -1,7 +1,9 @@
+import { FC } from 'react';
+
 import Slider from 'react-slick';
 
-import { FC } from 'react';
 import { Card } from './card';
+
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
@@ -15,25 +17,25 @@ const settings = {
         slidesToShow: 3,
         slidesToScroll: 3,
         infinite: true,
-        dots: true,
-      },
+        dots: true
+      }
     },
     {
       breakpoint: 768,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2,
-        initialSlide: 2,
-      },
+        initialSlide: 2
+      }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
+        slidesToScroll: 1
+      }
+    }
+  ]
 };
 
 export const SliderNFT: FC = () => {
