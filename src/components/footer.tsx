833b9a958a6e1002b1650423b950d68b42a0cc8f
Digberi <orkepuma@gmail.com>
2021-12-23 20:43:34 +0200

    add link for footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,9 @@ import { PoweredByQuipuswap } from '../icons/powered-by-quipuswap';
 export const Footer = () => {
   return (
     <div className="footer">
-      <PoweredByQuipuswap />
-
+      <a href='https://quipuswap.com/'>
+        <PoweredByQuipuswap />
+      </a>
       <div className="copyright">© 2021 Quipuswap. All rights reserved.</div>
     </div>
   );
