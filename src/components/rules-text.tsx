28a203a51ab16e272b3f9b70d7e2834bc1af0247
Artem Koziar <koziar.artem@gmail.com>
2021-12-24 22:57:15 +0200

    add nobr

--- src/components/rules-text.tsx
+++ src/components/rules-text.tsx
@@ -3,14 +3,15 @@ import React, { FC } from 'react';
 export const RulesText: FC = () => (
   <div className="rules-text">
     <h4 className="rules-title">Rules</h4>
-    <div className="rules-point">1. To participate in the lottery, you need to stake 200 QUIPU</div>
-    <div className="rules-point">2. Your stake will be frozen 24 hours</div>
+    <div className="rules-point">1. To&nbsp;participate in&nbsp;the lottery, you need to&nbsp;stake 200&nbsp;QUIPU</div>
+    <div className="rules-point">2. Your stake will be&nbsp;frozen 24&nbsp;hours.</div>
     <div className="rules-point">
-      3. The first 500 participants who will stake QUIPU receive our special NFT at once.
+      3. The first 500 participants who will stake QUIPU receive our special NFT at&nbsp;once.
     </div>
     <div className="rules-point">
-      4. A participant gets a chance to win: 70% - Common NFT, 20% - Rare NFT, 10% Epic NFT
+      4. A&nbsp;participant gets a&nbsp;chance to&nbsp;win: <span className="nobr">70%&nbsp;&mdash; Common NFT,</span>{' '}
+      <span className="nobr">20%&nbsp;&mdash; Rare NFT,</span> <span className="nobr">10% Epic NFT.</span>
     </div>
-    <div className="rules-point">5. After 24 hours, your QUIPU will be claimable back.</div>
+    <div className="rules-point">5. After 24&nbsp;hours, your QUIPU will be&nbsp;claimable back.</div>
   </div>
 );
