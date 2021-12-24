6ebca7240589bb219c196140b3b63a78582301b1
Artem Koziar <koziar.artem@gmail.com>
2021-12-24 14:39:38 +0200

    new contract

--- src/hooks/useContracts.ts
+++ src/hooks/useContracts.ts
@@ -8,7 +8,7 @@ import { QsTokenContract } from '../api/qs-token-contract';
 import { useAccountPkh, useTezos } from '../connect-wallet/utils/dapp';
 import { NftToken } from '../interfaces/NftToken';
 
-const DISTRIBUTOR_CONTRACT = 'KT1KUJ5tiYAS6X2tDszpL4vEUVNxmcsu8f4e';
+const DISTRIBUTOR_CONTRACT = 'KT1CeqbpeKDYS7uaGgKvreBDShjG8WKwEagx';
 // eslint-disable-next-line no-console
 console.log('DISTRIBUTOR_CONTRACT', DISTRIBUTOR_CONTRACT);
 
