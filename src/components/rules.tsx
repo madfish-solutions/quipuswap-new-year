8384e2823a3517d28bea7080d735b9de6668066f
Artem Koziar <koziar.artem@gmail.com>
2021-12-24 16:21:51 +0200

    add Started

--- src/components/rules.tsx
+++ src/components/rules.tsx
@@ -26,7 +26,7 @@ export const Rules: FC<Props> = ({
   const [disabled, setDisabled] = useState(true);
 
   const handleDistributionTimerEnd = () => {
-    setDistributionLabel(null);
+    setDistributionLabel('Started');
     setDisabled(false);
   };
 
@@ -38,7 +38,7 @@ export const Rules: FC<Props> = ({
           <div className="rules-point">1. To participate in the lottery, you need to stake 200 QUIPU</div>
           <div className="rules-point">2. Your stake will be frozen 24 hours</div>
           <div className="rules-point">
-            3. The first 200 participants who will stake QUIPU receive our special NFT at once.
+            3. The first 500 participants who will stake QUIPU receive our special NFT at once.
           </div>
           <div className="rules-point">
             4. A participant gets a chance to win: 70% - Common NFT, 20% - Rare NFT, 10% Epic NFT
